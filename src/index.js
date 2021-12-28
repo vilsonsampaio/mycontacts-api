b5b565fea685b243570aa74502d83ac031e8f843
Vilson Sampaio <vilson.sampaio04@gmail.com>
2021-12-28 20:30:44 -0300

    aula 04.06: criando controller de contatos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('Hello World!');
-});
+app.use(routes);
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
