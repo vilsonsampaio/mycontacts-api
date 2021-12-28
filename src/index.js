322b0a2dc68488a2ae3b1b86a1762ac2f7315cb6
Vilson Sampaio <vilson.sampaio04@gmail.com>
2021-12-28 17:26:04 -0300

    aula 04.05: eslint e editorconfig

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ app.get('/', (request, response) => {
   response.send('Hello World!');
 });
 
-app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'))
+app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
