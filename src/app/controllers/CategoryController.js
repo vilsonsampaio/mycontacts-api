558d5b79764e5b361d2e2d44ccea95e6867484d1
Vilson Sampaio <vilson.sampaio04@gmail.com>
2022-01-06 00:32:30 -0300

    aula 04.25: criando o modulo de categorias

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,23 @@
+const CategoryRepository = require('../repositories/CategoryRepository');
+
+class CategoryController {
+  async index(request, response) {
+    const categories = await CategoryRepository.findAll();
+
+    response.json(categories);
+  }
+
+  async store(request, response) {
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoryRepository.store({ name });
+
+    response.json(category);
+  }
+}
+
+module.exports = new CategoryController();
