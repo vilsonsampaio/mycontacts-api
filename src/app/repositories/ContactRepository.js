f336fddbca4923d07ebcf6c802099e0b75c79c9b
Vilson Sampaio <vilson.sampaio04@gmail.com>
2022-01-05 22:43:51 -0300

    aula 04.21: refatorando metodos de find

--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -20,27 +20,29 @@ let contacts = [
 ];
 
 class ContactRepository {
-  findAll() {
-    return new Promise((resolve) => resolve(contacts));
+  async findAll() {
+    const rows = await db.query('SELECT * FROM contacts');
+
+    return rows;
   }
 
-  findById(id) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.id === id),
-    ));
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+
+    return row;
   }
 
-  findByEmail(email) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.email === email),
-    ));
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email]);
+
+    return row;
   }
 
   async create({
     name, email, phone, category_id,
   }) {
     const [row] = await db.query(`
-      INSERT INTO contacts(name, email, phone)
+      INSERT INTO contacts(name, email, phone, category_id)
       VALUES($1, $2, $3, $4)
       RETURNING *
     `, [name, email, phone, category_id]);
